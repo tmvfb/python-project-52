ae1442c649567b8a869203aa769f9d7d13b184e4
tmvfb <darestade@gmail.com>
2023-05-18 17:57:54 +0300

    add website

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/tmvfb/python-project-52/workflows/hexlet-check/badge.svg)](https://github.com/tmvfb/python-project-52/actions)
+
+[![Website https://python-project-52-production-ae52.up.railway.app/](https://img.shields.io/website-up-down-green-red/https/https://python-project-52-production-ae52.up.railway.app.svg)](https://python-project-52-production-ae52.up.railway.app/)
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,8 +27,11 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['webserver', 'localhost', '127.0.0.1', '0.0.0.0']
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS = [
+    'webserver', 'localhost', '127.0.0.1', '0.0.0.0',
+    'python-project-52-production-ae52.up.railway.app'
+]
 
 
 # Application definition
