97c39705862edcaaf8db4121b89f850b053638f4
tmvfb <darestade@gmail.com>
2023-05-31 00:46:40 +0300

    bad implementation of night mode

---
+++ static/theme_toggle.js
@@ -0,0 +1,9 @@
+$("input[id='lightSwitch']").on("change", function() {
+  if ($("html").attr("data-bs-theme") === "light") {
+    $("html").attr("data-bs-theme", "dark");
+  } else if ($("html").attr("data-bs-theme") === "dark") {
+    $("html").attr("data-bs-theme", "light");
+  } else {
+    $("html").attr("data-bs-theme", "light");
+  }
+});
