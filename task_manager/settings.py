253c831ddf62870cf253948d1537d37753155d49
Igor Kvachenok <116455436+tmvfb@users.noreply.github.com>
2023-07-12 13:58:07 +0300

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = bool(os.getenv('DEBUG'))
 ALLOWED_HOSTS = [
     'webserver', 'localhost', '127.0.0.1', '0.0.0.0',
     'python-project-52-production-ae52.up.railway.app'
